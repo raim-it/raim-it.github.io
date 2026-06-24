b54b4fbebf17ab6190fa1ebfd641771775a376a9
raim-it <raimbek9@bk.ru>
2026-06-24 19:25:45 +0500

    Create script.js

---
+++ wedding-invitation/script.js
@@ -0,0 +1,41 @@
+const weddingDate = new Date("September 12, 2026 18:00:00");
+
+function updateCountdown(){
+
+const now = new Date();
+
+const diff = weddingDate - now;
+
+const days = Math.floor(diff/(1000*60*60*24));
+
+const hours = Math.floor(diff/(1000*60*60)%24);
+
+const minutes = Math.floor(diff/(1000*60)%60);
+
+document.getElementById("countdown").innerHTML = `
+
+<div>
+${days}
+<br>
+Days
+</div>
+
+<div>
+${hours}
+<br>
+Hours
+</div>
+
+<div>
+${minutes}
+<br>
+Minutes
+</div>
+
+`;
+
+}
+
+updateCountdown();
+
+setInterval(updateCountdown,1000);
